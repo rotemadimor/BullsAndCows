f49bd8f9ed7303f0b7f5b17029bfe4d3b719fc99
NoaVassal <noavassal@gmail.com>
2025-05-13 17:37:32 +0300

    res and guess

---
+++ Guess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02
+{
+    internal class Guess<T>
+    {
+        private T m_guessFromUser { get; set; }
+        private Result m_result;
+        //private bool m_isWin { get; set; }
+
+        public Guess(T i_guessFromUser, int i_bulls, int i_cows /*,bool i_isWin*/)
+        {
+            m_guessFromUser = i_guessFromUser;
+            m_result = new Result( i_bulls, i_cows );
+            //m_isWin = i_isWin;
+        }
+    }
+}
---
+++ Result.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02
+{
+    internal struct Result
+    {
+        private int m_bulls { get; set; }
+        private int m_cows { get; set; }
+
+        public Result (int i_bulls, int i_cows)
+        {
+            m_bulls = i_bulls;
+            m_cows = i_cows;
+        }
+    }
+}
