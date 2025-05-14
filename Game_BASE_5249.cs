3360ef37166521af1c0440b0ad8d5f7577652ecd
rotemadimor <rotemadimor@gmail.com>
2025-05-14 16:05:59 +0300

    BACKUP files

---
+++ Game_BASE_5249.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02
+{
+    internal class Game
+    {
+        private GameUI m_gameUI;
+        private GameLogic m_gameLogic;
+
+
+        public void Start()
+        {
+            m_gameUI.Start();
+
+            GameLoop();
+        }
+
+        public void GameLoop()
+        {
+            m_gameUI.Start();
+        }
+
+    }
+}
