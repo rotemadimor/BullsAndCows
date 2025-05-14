b3355f13456fc66676e256639379383f2abd6e62
rotemadimor <rotemadimor@gmail.com>
2025-05-14 14:37:49 +0300

    untracked files on noa: 264f000 result and guess

---
+++ Result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02
+{
+    internal struct Result
+    {
+        public int Bulls { get; set; }
+        public int Cows { get; set; }
+        public Result(int i_bulls, int i_cows)
+        {
+            Bulls = i_bulls;
+            Cows = i_cows;
+        }
+    }
+}
