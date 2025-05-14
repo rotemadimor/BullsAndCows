8f0af516664ccff2330f6bc8a00bfc3902be4640
rotemadimor <rotemadimor@gmail.com>
2025-05-14 15:43:08 +0300

    recoverd Game, GameLogic and GameUI.cs

---
+++ GameLogic.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Ex02
+{
+    internal class GameLogic
+    {
+        private List<Guess<T>> m_listOfGuesses;
+        private int m_numberOfGusses;
+        private Guess<T> m_correctAnswer;
+
+        public List<Guess<T>> ListOfGuesses { get; set; }
+        public int NumberOfGuesses { get; set; }
+        public Guess<T> CorrectAnswer { get; }
+
+        public GameLogic(int i_numberOfGuesses, List<T> i_itemsToChooseFrom)
+        {
+            setRandomCorrectAnswer(i_itemsToChooseFrom);
+            NumberOfGuesses = i_numberOfGuesses;
+            m_listOfGuesses = new List<Guess<T>>(NumberOfGuesses);
+        }
+
+        public bool addGuessToList(List<T> i_guessFromUser, List<T> i_itemsToChooseFrom)
+        {
+            bool isAddedToList = false;
+            Guess<T> guess = new Guess<T>(i_guessFromUser, i_itemsToChooseFrom);
+
+            if(guess.GuessedSequence.Count != 0)
+            {
+                checkHowManyCowsAndBulls(i_guessFromUser, guess.ResultOfGuess.Bulls, guess.ResultOfGuess.Cows);
+                m_listOfGuesses.Add(guess);
+                isAddedToList = true;
+            }
+            return isAddedToList;
+        }
+        private void setRandomCorrectAnswer(List<T> i_itemsToChooseFrom)
+        {
+            Random random = new Random();
+            m_correctAnswer = new Guess<T>(i_itemsToChooseFrom, random);
+        }
+
+        public bool IsTheCurrentGuessAWin(Guess<T> i_guessFromUser)
+        {
+            bool isTheCurrentGuess = false;
+            if (i_guessFromUser.ResultOfGuess.Bulls == 4) 
+            {
+                isTheCurrentGuess = true;
+            }
+            return isTheCurrentGuess;
+        }
+        public void checkHowManyCowsAndBulls(List<T> i_guessFromUser, int bulls, int cows)
+        {
+            for (int index = 0; index < i_guessFromUser.Count; index++)
+            {
+                if (isCellBull(i_guessFromUser, index))
+                {
+                    bulls++;
+                }
+                else if (isCellCow(i_guessFromUser, index))
+                {
+                    cows++;
+                }
+            }
+        }
+        private bool isCellCow(List<T> i_guessFromUser, int index)
+        {
+            return i_guessFromUser.Contains(m_correctAnswer.GuessedSequence[index]);
+        }
+
+        private bool isCellBull(List<T> i_guessFromUser, int index)
+        {
+            return i_guessFromUser[index].Equals(m_correctAnswer.GuessedSequence[index]);
+        }
+
+        public bool IsFailedGame()
+        {
+            bool isFailedGame = false;
+            if(m_listOfGuesses.Count == m_numberOfGusses && !IsTheCurrentGuessAWin(ListOfGuesses[m_numberOfGusses-1]))
+            {
+                isFailedGame = true;
+            }
+            return isFailedGame;
+        }
+
+        public bool IsUserQuitGame(List<T> i_inputFromUser, List<T> i_quitingInput)
+        {
+            bool isUserQuitGame = false;    
+            if(i_inputFromUser.Equals(i_quitingInput))
+            {
+                isUserQuitGame = true;
+            }
+            return isUserQuitGame;
+        }
+
+    }
+}
---
+++ GameUI.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ex02.ConsoleUtils;
+
+
+namespace Ex02
+{
+    internal class GameUI
+    {
+        private int m_numberOfGuesses;
+
+        public int NumberOfGuesses { get; set; }
+
+        public void Start()
+        {
+            Screen.Clear();
+            Console.WriteLine("Type number of guesses:");
+            NumberOfGuesses = Convert.ToInt32(Console.ReadLine());
+            printEmptyTable();
+        }
+
+        public List<char> GetGuessInputFromUser()
+        {
+            Console.WriteLine("Please type your next guess 'A' to 'H' or 'Q' to quit");
+            string input = (Console.ReadLine());
+            return input.ToList();
+        }
+
+        private void printTableWithGuesses(List<Guess<char>> i_guesses)
+        {
+            int numberOfEmptyCells = NumberOfGuesses - i_guesses.Count;
+            PrintGuessesToTable(i_guesses);
+            for (int i = 0; i < numberOfEmptyCells; i++)
+            {
+                Console.WriteLine("|         |          |");
+                Console.WriteLine("|=========|==========|");
+            }
+            Console.ReadLine();
+
+        }
+
+        private void printEmptyTable()
+        {
+            Screen.Clear();
+            printHeadlineOfTable();
+
+            for (int i = 0; i < NumberOfGuesses; i++)
+            {
+                Console.WriteLine("|         |          |");
+                Console.WriteLine("|=========|==========|");
+            }
+            Console.ReadLine();
+        }
+
+        private static void printHeadlineOfTable()
+        {
+            Console.WriteLine("Current board status:");
+            Console.WriteLine("|Pins:    |Result:   |");
+            Console.WriteLine("|=========|==========|");
+            Console.WriteLine("| # # # # |          |");
+            Console.WriteLine("|=========|==========|");
+        }
+
+        public void PrintGuessesToTable(List<Guess<char>> i_guesses)
+        {
+            foreach(Guess<char> guessToPrint in i_guesses)
+            {
+                printGuess(guessToPrint);
+            }
+        }
+
+        private static void printGuess(Guess<char> i_guessToPrint)
+        {
+            string printableGuessSequence = printableUserInputToTable(i_guessToPrint);
+            string printableResult = printableResultToTable(i_guessToPrint.ResultOfGuess);
+
+            Console.WriteLine($"|{printableGuessSequence}|{printableResult}|");
+            Console.WriteLine("|=========|==========|");
+        }
+
+        private static string printableUserInputToTable(Guess<char> i_guessToPrint)
+        {
+            string guessSequence = i_guessToPrint.GuessedSequence.ToString();
+            return addSpacesBetweenChars(guessSequence);  
+        }
+
+        private static string printableResultToTable(Result i_resultToPrint)
+        {
+            List<char> printableResult = new List<char>(4);
+            for (int b = 0; b < i_resultToPrint.Bulls; b++)
+            {
+                printableResult.Append('V');
+            }
+            for (int c = 0; c < i_resultToPrint.Cows; c++)
+            {
+                printableResult.Append('X');
+            }
+            return addSpacesBetweenChars(printableResult.ToString());
+        }
+
+        public void PrintGameSummeryWin(List<Guess<char>> i_guesses)
+        {
+            Screen.Clear();
+            PrintGuessesToTable(i_guesses);
+            Console.WriteLine($"You guessed after {i_guesses.Count} steps!");
+        }
+
+        public void PrintGameSummeryLose(List<Guess<char>> i_guesses)
+        {
+            Screen.Clear();
+            PrintGuessesToTable(i_guesses);
+            Console.WriteLine("No more guesses allowed. You Lost.");
+        }
+
+        private static string addSpacesBetweenChars(string i_input)
+        {
+            return string.Join(" ", i_input.ToCharArray());
+        }
+    }
+}
